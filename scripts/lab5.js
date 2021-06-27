956f8057734d0e5ba095a6d1b0c3a75f749bfe32
nwahb063 <71499809+nwahb063@users.noreply.github.com>
2021-06-27 13:19:00 +0100

    Update lab5.js

--- scripts/lab5.js
+++ scripts/lab5.js
@@ -1,14 +1,7 @@
-// Function to verify that the phone number is correct.
-// Here, I validate for (12345), but you have to change that for a phone validation
-// Tutorials on Regular expressions
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions 
-// https://flaviocopes.com/javascript-regular-expressions/ 
-// Regular expressions can get complex, you can think in terms of a series of characters
-// or numbers 
+
 function validatePhone(txtPhone) {
     var a = document.getElementById(txtPhone).value;
-    // This filter asks for something like (12345), so parentheses with any number (at least 1)
-    // of digits
+    
     var filter = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
     if (filter.test(a)) {
         return true;
@@ -52,31 +45,27 @@ function validateCVV(txtCVV) {
     }
 } 
 
-// Using date restrictions on datepicker
-// Document of datepicker is here: https://api.jqueryui.com/datepicker/ 
-// The following code shows how to set specific dates to exclude, as well as Sundays (Day 0)
-// Make sure in your version that you associate Days to remove with Experts (e.g. John doesn't work Mondays)
-// var unavailableDates = ["06/29/2020","07/07/2020","07/10/2020"];
+
 
 const setDateFormat = "mm/dd/yy";
 
 function disableDates(date) {
-    // Sunday is Day 0, disable all Sundays
+   
 
     var crewDateSelected = $('input[name=crewchosen]:checked').val();
     var stylechoice = $('input[name=stylechoice]:checked').val();
     if (crewDateSelected && stylechoice) {
         if(crewDateSelected == 1) {
-            // Sunday and Monday
+           
             if (date.getDay() == 0 || date.getDay() == 1) {
                 return [false];
             }
-            //Tuesday
+           
         } else if (crewDateSelected == 2) {
             if (date.getDay() == 2) {
                 return [false];
             }
-            //Thursday
+           
         } else if (crewDateSelected == 3) {
             if (date.getDay() == 4) {
                 return [false];
@@ -85,15 +74,14 @@ function disableDates(date) {
         return [true];
     }
     return [false];
-    // var string = jQuery.datepicker.formatDate(setDateFormat, date);
-    // return [ unavailableDates.indexOf(string) == -1 ]
+    
 }
 
 
-// HERE, JQuery "LISTENING" starts
+
 $(document).ready(function(){
 
-    //Tooltip
+    
     $("#name").tooltip({
         position: {
             my: "center bottom", // the "anchor point" in the tooltip element
@@ -143,10 +131,7 @@ $(document).ready(function(){
     });
 
 
-    // phone validation, it calls validatePhone
-    // and also some feedback as an Alert + putting a value in the input that shows the format required
-    // the "addClass" will use the class "error" defined in style.css and add it to the phone input
-    // The "error" class in style.css defines yellow background and red foreground
+   
     $("#phone").on("change", function(){
         if (!validatePhone("phone")){
             $("#phone").addClass("is-invalid");
@@ -156,15 +141,7 @@ $(document).ready(function(){
         }
     });
 
-    // To change the style of the calender, look in jqueryui.com, under Themes, in the ThemeRoller Gallery 
-    // You can try different themes (the names are under the calendars) / This is Excite Bike 
-    // To use a different theme you must include its css in your HTML file. 
-    // The one I included in my HTML is the Excite Bike, but you can try others
-
-    // Also, here is a good tutorial for playing with the datepicker in https://webkul.com/blog/jquery-datepicker/ 
-    // Datepicker is also documented as one of the widgets here: https://api.jqueryui.com/category/widgets/
-
-
+   
     var today = new Date();
 
     $( "#dateInput" ).datepicker(
@@ -172,7 +149,7 @@ $(document).ready(function(){
             dateFormat: setDateFormat,
             minDate: today,  
             maxDate: '+4M',
-            // used to disable some dates
+           
             beforeShowDay: $.datepicker.noWeekends,
             beforeShowDay: disableDates
         }   
